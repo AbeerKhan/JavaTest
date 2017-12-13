ff003b76723acb82d462d89b3a095a88ae272a6c
abeer <abeer@DESKTOP-40C64QH>
2017-12-13 12:34:11 -0500

    Getter and Setter in Java

--- Test-project/src/Ball.java
+++ Test-project/src/Ball.java
@@ -1,8 +1,20 @@
 
 public class Ball {
 
-	public String color;
-	public int size;
+	private String color;
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	private int size;
 	
 	
 }
--- Test-project/src/Main.java
+++ Test-project/src/Main.java
@@ -3,19 +3,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(addNumbers(1, 4));
 		
 		Ball myBall = new Ball();
-		myBall.color = "blue";
-		myBall.size = 24;
-		System.out.println(myBall.color);
+		myBall.setColor("Green");
+		myBall.setSize(34);
+		
+		System.out.println(myBall.getColor() + " " + myBall.getSize() + ".");
 	
-	}
-	
-	public static int addNumbers(int a, int b) {
-		int result = 0;
-		result = a + b;
-		return result ;
 	}
 
 }
